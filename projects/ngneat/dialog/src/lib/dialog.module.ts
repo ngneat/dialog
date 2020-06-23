e9c78d146f228c66fd06d933c08e43f2616275e7
tonivj5 <tonivj5@gmail.com>
2020-06-23 19:51:19 +0200

    fix(dialog-module): forRoot config is optional

--- projects/ngneat/dialog/src/lib/dialog.module.ts
+++ projects/ngneat/dialog/src/lib/dialog.module.ts
@@ -21,7 +21,7 @@ const BuiltIns = [BaseDialogComponent, SuccessDialogComponent, ConfirmDialogComp
   exports: [DialogComponent, DialogCloseDirective]
 })
 export class DialogModule {
-  static forRoot(config?: GlobalDialogConfig): ModuleWithProviders<DialogModule> {
+  static forRoot(config?: Partial<GlobalDialogConfig>): ModuleWithProviders<DialogModule> {
     return {
       ngModule: DialogModule,
       providers: [
