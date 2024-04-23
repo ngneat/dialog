73413db0e9ea7a4548f6239f1f4ba3a1ecb020ed
Nuriddin Yuldashev <huskforever@gmail.com>
2024-04-23 13:23:46 +0500

    fix: crypto does not work in http domain (specs)

--- libs/dialog/src/lib/dialog.utils.ts
+++ libs/dialog/src/lib/dialog.utils.ts
@@ -13,3 +13,11 @@ export function coerceCssPixelValue(value: any): string {
 
   return isString(value) ? value : `${value}px`;
 }
+
+export function generateUUID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0,
+      v = c == 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
